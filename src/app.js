80ea5141e3d942a1fb1890846d82c064e1236ae4
yutzi <kikilo20041212@gmail.com>
2020-07-02 02:01:34 +0800

    fix asyn issues

--- src/app.js
+++ src/app.js
@@ -1,179 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import firebase from "firebase/app";
-import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
-} from "@react-firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAgpVLf1iNb1RJmq3QBIuHdDnMUYObYqKo",
-  authDomain: "kiwords-c058b.firebaseapp.com",
-  databaseURL: "https://kiwords-c058b.firebaseio.com",
-  projectId: "kiwords-c058b",
-  storageBucket: "kiwords-c058b.appspot.com",
-  messagingSenderId: "959153092087",
-  appId: "1:959153092087:web:278807305aad9aa00caaf2",
-  measurementId: "G-1GYNCR5WYZ"
-};
-
+import Auth from "./firebaseAuth"
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      signIn: " ",
-      logIn: false,
-      userData: []
-    };
-
-    this.handleSignUpState = this.handleSignUpState.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.handleSignInState = this.handleSignInState.bind(this);
-    this.handleSignIn = this.handleSignIn.bind(this);
-    this.getLoginRes = this.getLoginRes.bind(this);
-  }
-
- 
-  componentDidMount() {
-    console.log('initial render detected')
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log("onAuthStateChanged?")
-
-      if (user) {
-        console.log("login")
-        this.setState({ logIn: true })
-      } else {
-        console.log("logout")
-        this.setState({ logIn: false })
-      }
-    });
-  } 
-
-  swithLogState() {
-    if (this.state.logIn) {
-      this.setState({logIn:true})
-    } else {
-      this.setState({logIn:false})
-    }
-  }
-
-
-  handleSignUpState(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSignUp(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-    firebase.auth().createUserWithEmailAndPassword(this.state.value, this.state.value)
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  handleSignInState(event) {
-    this.setState({ signIn: event.target.value });
-  }
-
-  handleSignIn(event) {
-    alert('A name was submitted: ' + this.state.signIn);
-    event.preventDefault();
-    firebase.auth().signInWithEmailAndPassword(this.state.signIn, this.state.signIn)
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-  
-
-
-
   render() {
     return (
-      <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
-        
-        <form onSubmit={this.handleSignUp}>
-          <input type="text" onChange={this.handleSignUpState} />
-          <input type="submit" value="sign upppp" />
-        </form>
-
-        <form onSubmit={this.handleSignIn}>
-          <input type="text" onChange={this.handleSignInState} />
-          <input type="submit" value="sign innnn" />
-        </form>
-
-
-        <div>
-          <button
-            onClick={() => {
-              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-              firebase.auth().signInWithPopup(googleAuthProvider);
-            }}
-          >
-            Sign In with Google
-        </button>
-          <button
-            onClick={() => {
-              const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
-              firebase.auth().signInWithPopup(facebookAuthProvider);
-            }}
-          >
-            Sign In with Facebook
-        </button>
-          <button
-            data-testid="signin-anon"
-            onClick={() => {
-              firebase.auth().signInAnonymously();
-            }}
-          >
-            Sign In Anonymously
-        </button>
-          <button
-            onClick={() => {
-              firebase.auth().signOut();
-            }}
-          >
-            Sign Out
-        </button>
-          <FirebaseAuthConsumer>
-
-            {({ isSignedIn, user, providerId }) => {
-
-              return (
-                <pre style={{ height: 300, overflow: "auto" }}>
-                  {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-                  {/* I cant set state and store login data here !*/}
-                </pre>
-              );
-            }}
-          </FirebaseAuthConsumer>
-          <div>
-            <IfFirebaseAuthed>
-              {() => {
-                return <div>You are authenticated</div>;
-              }}
-            </IfFirebaseAuthed>
-            <IfFirebaseAuthedAnd
-              filter={({ providerId }) => providerId !== "anonymous"}
-            >
-              {({ providerId }) => {
-                return <div>You are authenticated with {providerId}</div>;
-              }}
-            </IfFirebaseAuthedAnd>
-          </div>
-        </div>
-      </FirebaseAuthProvider>
+      <Auth />
     )
   }
 }
@@ -181,9 +13,7 @@ class App extends React.Component {
 
 
 
-
-
-
-
 //=========================
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root"));
---
+++ src/firebaseAuth.js
@@ -0,0 +1,168 @@
+import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAgpVLf1iNb1RJmq3QBIuHdDnMUYObYqKo",
+  authDomain: "kiwords-c058b.firebaseapp.com",
+  databaseURL: "https://kiwords-c058b.firebaseio.com",
+  projectId: "kiwords-c058b",
+  storageBucket: "kiwords-c058b.appspot.com",
+  messagingSenderId: "959153092087",
+  appId: "1:959153092087:web:278807305aad9aa00caaf2",
+  measurementId: "G-1GYNCR5WYZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+class Auth extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: "", //for passing input data 
+      signIn: "", //for passing input data 
+      logIn: false,
+      userData: []
+    };
+
+    this.handleSignUpState = this.handleSignUpState.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleSignInState = this.handleSignInState.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
+
+  }
+
+
+  componentDidMount() {
+    console.log('initial render detected')
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log("onAuthStateChanged?")
+
+      if (user) {
+        console.log("login")
+        this.setState({ logIn: true })
+
+        var user = firebase.auth().currentUser;
+        if (user != null) {
+          this.setState({ userData: [user]}) 
+        } else {
+          console.log("user is null")
+        }
+      } else {
+        console.log("logout")
+        this.setState({ logIn: false })
+        this.setState({ userData: [] }) 
+      }
+    }); 
+
+
+
+  }
+
+  swithLogState() {
+    if (this.state.logIn) {
+      this.setState({ logIn: true })
+    } else {
+      this.setState({ logIn: false })
+    }
+  }
+
+
+  handleSignUpState(event) {
+    this.setState({ signUp: event.target.value });
+  }
+
+  handleSignUp(event) {
+
+      alert('A name was submitted: ' + this.state.signUp);
+      event.preventDefault();
+      firebase.auth().createUserWithEmailAndPassword(this.state.signUp, this.state.signUp)
+        .then((res) => {
+          console.log(res)
+          this.setState({ userData: [res] })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    
+  }
+
+  handleSignInState(event) {
+    this.setState({ signIn: event.target.value });
+  }
+
+  handleSignIn(event) {
+
+      alert('A name was submitted: ' + this.state.signIn);
+      event.preventDefault();
+      firebase.auth().signInWithEmailAndPassword(this.state.signIn, this.state.signIn)
+        .then((res) => {
+          console.log(res)
+          this.setState({ userData: [res] })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    
+  }
+
+
+  render() {
+    return (
+      <div>
+
+        <form onSubmit={this.handleSignUp}>
+          <input type="text" onChange={() => { this.handleSignUpState }} />
+          <input type="submit" value="sign upppp" />
+        </form>
+
+        <form onSubmit={this.handleSignIn}>
+          <input type="text" onChange={() => { this.handleSignInState }} />
+          <input type="submit" value="sign innnn" />
+        </form>
+      {/* 目前卡在上面兩個選項，無法阻擋重複登入者（會重新跳一次頁面，且沒有出現警告標示） */}
+
+        <div>
+          <button
+            onClick={() => {
+              if (this.state.logIn) {
+                alert('you are already log in!')
+                return
+              }
+              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+              firebase.auth().signInWithPopup(googleAuthProvider)
+                .then((res) => {
+                  console.log(res)
+                  this.setState({ userData: [res] })
+                })
+                .catch((err) => { console.log(err) })
+            }}>Sign In with Google
+          </button>
+
+          <button
+            onClick={() => {
+              if (this.state.logIn) {
+                alert('you are already log in!')
+                return
+              }
+              const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+              firebase.auth().signInWithPopup(facebookAuthProvider);
+            }}>Sign In with Facebook
+          </button>
+          <button
+            onClick={() => {
+              firebase.auth().signOut();
+            }}>Sign Out
+          </button>
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+export default Auth
